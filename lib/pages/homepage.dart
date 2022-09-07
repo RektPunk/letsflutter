ffba147c661df0f5760cd9a840033354fda7cdfd
RektPunk <rektpunk@gmail.com>
2022-09-07 23:37:59 +0900

    feat - apply container and sized box

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -11,8 +11,26 @@ class MyHompage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('appBar title')
       ),
-      body: const Center(
-        child: Text('body title'),
+      body: Container(
+        color: Colors.purple,
+        alignment: Alignment.centerLeft,
+        // alignment: const Alignment(1, 0),
+        width: 200,
+        height: 100,
+        // padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30, 
+          vertical: 30
+        ),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 30, 
+          vertical: 20
+        ),
+        child: const SizedBox(
+          width: 200,
+          height: 200,
+          child: Text('app title'),
+        )
       ),
     );
   }
