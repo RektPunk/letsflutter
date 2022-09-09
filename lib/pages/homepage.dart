dcab6c40aae0e2736355aaa7e17bfcf21bb1a824
RektPunk <rektpunk@gmail.com>
2022-09-09 20:26:10 +0900

    feat - add margin and padding

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -13,24 +13,28 @@ class MyHompage extends StatelessWidget {
       ),
       body: Container(
         color: Colors.purple,
-        alignment: Alignment.centerLeft,
-        // alignment: const Alignment(1, 0),
-        width: 200,
-        height: 100,
-        // padding: const EdgeInsets.all(10),
-        padding: const EdgeInsets.symmetric(
-          horizontal: 30, 
-          vertical: 30
-        ),
-        margin: const EdgeInsets.symmetric(
-          horizontal: 30, 
-          vertical: 20
-        ),
-        child: const SizedBox(
-          width: 200,
-          height: 200,
-          child: Text('app title'),
-        )
+        // alignment: Alignment.centerLeft,
+        // alignment: const Alignment(2, 2),
+        // width: 200,
+        // height: 100,
+        // padding: const EdgeInsets.all(30),
+        // padding: const EdgeInsets.symmetric(
+        //   horizontal: 10, 
+        //   vertical: 30,
+        // ),
+        // padding: const EdgeInsets.fromLTRB(10, 20, 30, 40),
+        // padding: const EdgeInsets.only(left: 30),
+        margin: const EdgeInsets.all(30),
+        // margin: const EdgeInsets.symmetric(
+        //   horizontal: 30, 
+        //   vertical: 20,
+        // ),
+        // child: const SizedBox(
+        //   width: 200,
+        //   height: 200,
+        //   child: Text('app title'),
+        // )
+        // child: const Text('app title'),
       ),
     );
   }
